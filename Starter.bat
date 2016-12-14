8c38880a9644a1cac8c37a53c391052699d0c136
joshua mims <Speed9425@gmail.com>
2016-12-14 16:16:52 -0600

    Add files via upload

---
+++ Starter.bat
@@ -0,0 +1,16 @@
+@echo off
+IF EXIST "paperclip.jar" (
+:begin
+echo Auto Restart Script Initialized
+java -Xms1G -Xmx2G -jar paperclip.jar -d64 -o true
+PAUSE
+goto begin
+REM Do one thing
+) ELSE (
+powershell -Command "Invoke-WebRequest https://ci.destroystokyo.com/job/PaperSpigot/lastSuccessfulBuild/artifact/paperclip.jar -OutFile paperclip.jar"
+echo Auto Restart Script Initialized
+java -Xms1G -Xmx2G -jar paperclip.jar -d64 -o true
+PAUSE
+goto begin
+REM Do another thing
+)
