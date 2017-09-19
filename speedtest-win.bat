691691073f06e682f3237d9b862343fa050ced79
joshua mims <Speed9425@gmail.com>
2017-09-19 15:31:53 -0500

    add windows variant of speedtest

---
+++ speedtest-win.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+cd C:\users\%USERNAME%
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py -OutFile speedtest.py"
+.\speedtest.py
+del /f speedtest.py
+endlocal
+pause
