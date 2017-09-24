62a6cfa8363052a2672c0fef0ecf1cb83e820d6a
joshua mims <speed9425@gmail.com>
2017-09-24 04:23:32 -0500

    update scripts(starting using idea for ease)

--- Starter.bat
+++ Starter.bat
@@ -1,8 +1,6 @@
 @echo off
 IF EXIST "paperclip.jar" (
 :begin
-RAM=1G
-MAXRAM=2G
 echo Auto Restart Script Initialized
 java -Xms1G -Xmx2G -jar paperclip.jar
 PAUSE
@@ -10,7 +8,7 @@ goto begin
 ) ELSE (
 powershell -Command "Invoke-WebRequest https://ci.destroystokyo.com/job/PaperSpigot/lastSuccessfulBuild/artifact/paperclip.jar -OutFile paperclip.jar"
 echo Auto Restart Script Initialized
-java -Xms$RAM -Xmx$MAXRAM -jar paperclip.jar
+java -Xms1G -Xmx2G -jar paperclip.jar
 PAUSE
 goto begin
 )
