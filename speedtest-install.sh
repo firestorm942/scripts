c9e4f870a801dd792db481d3e76a4933f8bdccf3
joshua mims <speed9425@gmail.com>
2017-09-24 03:45:08 -0500

    update scripts(starting using idea for ease)

--- speedtest-install.sh
+++ speedtest-install.sh
@@ -13,7 +13,7 @@ if [ ! -e /usr/bin/speedtest ]
 then
     echo "Installing speedtest. Press [Enter] to continue/n or [ctrl+c] to cancel."
     wget -qc https://raw.githubusercontent.com/firestorm942/scripts/master/speedtest
-    sudo cp speedtest /usr/bin/speedtest
+    sudo mv speedtest /usr/bin/speedtest
     echo "Speedtest installed successfully!"
     else
     read -p "<Do you wish to remove speedtest?> : y/N" CONDITION;
