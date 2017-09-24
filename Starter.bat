8cefe0e66b803807c770fe60571e99cde9211dcb
joshua mims <Speed9425@gmail.com>
2017-09-24 01:38:06 -0500

    Update Starter.bat

--- Starter.bat
+++ Starter.bat
@@ -9,7 +9,7 @@ REM Do one thing
 ) ELSE (
 powershell -Command "Invoke-WebRequest https://ci.destroystokyo.com/job/PaperSpigot/lastSuccessfulBuild/artifact/paperclip.jar -OutFile paperclip.jar"
 echo Auto Restart Script Initialized
-java -Xms1G -Xmx2G -jar paperclip.jar -d64 -o true
+java -Xms1G -Xmx2G -jar paperclip.jar
 PAUSE
 goto begin
 REM Do another thing
