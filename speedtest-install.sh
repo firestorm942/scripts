3ec42f8ad4aa6c67354916a2e1df0605bba2b8a8
joshua mims <speed9425@gmail.com>
2017-09-24 03:26:40 -0500

    update scripts(starting using idea for ease)

--- speedtest-install.sh
+++ speedtest-install.sh
@@ -2,12 +2,10 @@
 echo "checking if python is installed"
 if command -v python > /dev/null 2>&1; then
     echo "python installed, resuming"
-    Main
 else
     echo "Installing python"
     sudo apt install -y -qq python
     echo "Install successful"
-    Main
 fi
 function Main {
 if [ -e /usr/bin/speedtest ]
@@ -23,9 +21,10 @@ else
     read -p "<Do you wish to install speedtest?> : y/N" CONDITION;
     if [ "$CONDITION" == "y" ]; then
     wget -qc https://raw.githubusercontent.com/firestorm942/scripts/master/speedtest
-    sudo mv speedtest /usr/bin/speedtest
+    sudo cp speedtest /usr/bin/speedtest
     echo "Speedtest installed successfully!"
 fi
 fi
 fi
 }
+Main
