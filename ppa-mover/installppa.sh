68904ea6d11968e44b4e1afd016d814cfd6f0303
firestorm942 <speed9425@gmail.com>
2022-02-06 14:01:56 -0600

    upkeep

---
+++ ppa-mover/installppa.sh
@@ -0,0 +1,8 @@
+sudo apt-add-repository ppa:graphics-drivers/ppa
+sudo apt-add-repository ppa:jonathonf/barrier
+sudo apt-add-repository ppa:flexiondotorg/mangohud
+sudo apt-add-repository ppa:mmk2410/intellij-idea
+sudo apt-add-repository ppa:x2go/stable
+sudo apt-add-repository ppa:likemartinma/htop
+sudo apt-add-repository ppa:obsproject/obs-studio
+sudo apt-add-repository ppa:lutris-team/lutris
--- speedtest-win.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-cd C:\users\%USERNAME%\AppData\Roaming
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py -OutFile speedtest.py"
-.\speedtest.py
-del /f speedtest.py
-endlocal
-pause
